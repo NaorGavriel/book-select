6a9be951286837783d8c126d6aff6d1fde521f2a
NaorGavriel <117026659+NaorGavriel@users.noreply.github.com>
2026-02-27 09:24:35 +0200

    implmented backend route that returns all books read by a user

--- server/app/routes/user_book.py
+++ server/app/routes/user_book.py
@@ -1,8 +1,8 @@
 from fastapi import HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
-from app.services.user_book import add_user_book
+from app.services.user_book import add_user_book, get_books_of_user
 from app.db import get_db
-from app.schemas.user_book import UserBookCreateSchema
+from app.schemas.user_book import UserBookCreateSchema, UserBookResponse
 from app.models.user import User
 from app.services.auth import get_current_user
 
@@ -15,6 +15,24 @@ async def post_user_book(user_book_data : UserBookCreateSchema, db: Session = De
         author = user_book_data.author
         result = add_user_book(db=db, title=title, author=author, user_id=user.id)
         return result
+    except Exception as e:
+        raise HTTPException(
+            status_code=404,
+            detail=str(e)
+            )
+    
+
+@router.get("/", response_model=list[UserBookResponse])
+async def get_user_books(db: Session = Depends(get_db), user : User = Depends(get_current_user)):
+    try:
+        books = get_books_of_user(db=db, user_id=user.id)
+        if books == []:
+            raise HTTPException(
+                status_code=404,
+                detail=str(e)
+                )    
+            
+        return books
     except Exception as e:
         raise HTTPException(
             status_code=404,
--- server/app/schemas/user_book.py
+++ server/app/schemas/user_book.py
@@ -7,3 +7,13 @@ class UserBookCreateSchema(BaseModel):
     class Config:
         from_attributes = True
 
+
+class UserBookResponse(BaseModel):
+    title : str
+    authors : list[str]
+    description : str
+    genres : list[str]
+
+    model_config = {
+        "from_attributes": True
+    }
